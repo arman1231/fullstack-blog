764d06ec1ab37fa5d33f4e1c40894d23d8300332
Arman Cherkharov <arman1231@yandex.ru>
2023-03-30 14:55:49 +0300

    add upload files feature & add unified error validator

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import { registerValidation, loginValidation } from "./validations/auth.js";
 import checkAuth from "./utils/checkAuth.js";
 import { getMe, login, register } from "./controllers/UserController.js";
 import * as PostController from "./controllers/PostController.js";
+import * as UserController from "./controllers/UserController.js";
 import { postCreateValidation } from "./validations/post.js";
+import multer from "multer";
+import handleValidationErrors from "./utils/handleValidationErrors.js";
 
 mongoose
   .connect("mongodb://localhost:27017/fullstack_blog")
@@ -18,15 +21,33 @@ mongoose
 const app = express();
 const PORT = 4444;
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 app.get("/", (req, res) => {
   res.send("https://github.com/arman1231/fullstack-blog");
 });
 
-app.post("/auth/login", loginValidation, login);
-app.post("/auth/register", registerValidation, register);
-app.get("/auth/me", checkAuth, getMe);
+app.post("/auth/login", loginValidation, handleValidationErrors, UserController.login);
+app.post("/auth/register", registerValidation, handleValidationErrors,UserController.register);
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) =>{
+  res.json({
+    url: `uploads/${req.file.originalname}`
+  })
+})
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
