998ccfad12408f8a7c9e4a4b2615866adec5971d
Arman Cherkharov <arman1231@yandex.ru>
2023-03-30 17:02:46 +0300

    add redux toolkit basic setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from 'cors'
 import { registerValidation, loginValidation } from "./validations/auth.js";
 import { postCreateValidation } from "./validations/post.js";
 import multer from "multer";
@@ -30,6 +31,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors())
 app.use('/uploads', express.static('uploads'));
 
 app.get("/", (req, res) => {
