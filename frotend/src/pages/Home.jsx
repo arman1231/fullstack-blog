b19e025967faed132dccea4cd60167fd3f525592
Arman Cherkharov <arman1231@yandex.ru>
2023-03-31 11:13:55 +0300

    load fullpost dinamically

--- frotend/src/pages/Home.jsx
+++ frotend/src/pages/Home.jsx
@@ -33,9 +33,9 @@ export const Home = () => {
         <Grid xs={8} item>
           {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => isPostsLoading ? (<Post key={index} isLoading={true} />) : (
             <Post
-              id={obj._id}
+              _id={obj._id}
               title={obj.title}
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+              imageUrl={obj.imageUrl}
               user={obj.user}
               createdAt={obj.createdAt}
               viewsCount={obj.viewsCount}
